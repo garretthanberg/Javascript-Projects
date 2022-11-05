cfbfee8c4ae3eac936cd65e57cecbc10f74403cc
garretthanberg <garretthanbergphoto@gmail.com>
2022-11-04 20:49:18 -0700

    Created index.html and main.js

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var Sum1 = "This is the beginning of the string,"; //Here I am creating a string variable for Sum1
+
+var Sum2 = " and this is the end of the string."; //Here I am creating a string variable for Sum2
+
+document.write(Sum1 + Sum2); //Here I am concatenating Sum1 together with Sum2
+
+document.write(12,000 + 42); //Here I am displaying what 12,000 plus 42 equals with an expression 
+
+var G = "Basic Javascript Project Submission"; //Here I am creating the variable "G"
+
+window.alert(G); //Here I created an alert with the variable "G"
