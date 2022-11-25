3889d6929e353abaea45b4eb144d23fa08713576
garretthanberg <garretthanbergphoto@gmail.com>
2022-11-25 12:18:16 -0800

    Created HTML_4.html

--- Javascript/JS/Javascript_2.js
+++ Javascript/JS/Javascript_2.js
@@ -1,12 +1,17 @@
 function validateForm() {
     let x = document.forms["myForm"]["firstName"].value;
     let y = document.forms["myForm"]["lastName"].value;
+    let z = document.forms["myForm"]["phoneNumber"].value;
     if (x == "") {
         alert("First Name must be filled out");
         return false;
     }
     if (y == "") {
         alert("Last Name must be filled out");
+        return false;
+    }
+    if (z == "") {
+        alert("Phone Number must be filled out");
         return false;
     }
 }
